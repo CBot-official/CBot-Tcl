357daf5959890c3cdd61313a4d1fc9a564322034
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-05 16:16:45 +0100

    Create cbot.tcl

---
+++ cbot.tcl
@@ -0,0 +1,90 @@
+###############################################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+# Author : ComputerTech
+# Version: 0.0.1
+# Email  : computertech312@gmail.com
+# GitHub : https://github.com/computertech312
+#################################################
+# Description:
+#
+#
+#################################################
+# Notes:
+#     
+#
+#################################################
+
+### Start of configuration ###
+
+## Bot nick.
+set botnick "CBot"
+
+## Bot ident.
+set botident "CBot"
+
+## Bot realname.
+set botreal "CBot"
+
+## Bot server.
+set server "irc.address.org"
+
+## Bot port.
+set port "+6697"
+
+## Bot admin.
+set admin "Your-nick"
+
+### End of configuration ###
+
+
+proc ircsplit str {
+	set res {}
+	if {[string index $str 0] eq ":"} {
+		lappend res [string range $str 1 [set first [string first " " $str]]-1]
+		set str [string range $str 1+$first end]
+	} else {
+		lappend res {}
+	}
+	if {[set pos [string first " :" $str]] != -1} {
+		lappend res {*}[split [string range $str 0 ${pos}-1]]
+		lappend res [string range $str 2+$pos end]
+	} else {
+		lappend res {*}[split $str]
+	}
+	return $res
+}
+
+package require http
+package require tls
+
+if {[string index $::port 0] == "+"} {
+ set sockChan [tls::socket $::server $::port]
+} else {
+ set sockChan [socket $::server $::port]
+}
+fconfigure $sockChan -translation crlf -buffering line
+puts $sockChan "NICK $::botnick"
+puts $sockChan "USER $::botname * * :$::botident"
+while {![eof $sockChan]} {
+	set line [gets $sockChan]
+	puts ">> $line"
+	set lline [ircsplit $line]
+	set text [lindex $lline end]
+	set nick [lindex [split $lline !] 0]
+	set host [lindex [split $lline !] 1]
+	set type [lindex $lline 1]
+	set chan [lindex $lline 2]
+	set text [lindex $lline 2]
+	switch -nocase -- [lindex $lline 1] {
+		PING {puts $sockChan "PONG :[lindex $lline 2]"}
+		001 {puts $sockChan "JOIN $::home"}
+		433 {puts $sockChan "NICK $::alternick"}
+			}
+		}
